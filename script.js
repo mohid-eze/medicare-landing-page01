ac8ec88152ec1ced1c3efbcf543f82304fc7b13e
mohid-eze <mohidkailash@gmail.com>
2026-03-06 13:03:03 +0500

    First commit - MEDICARE project

---
+++ script.js
@@ -0,0 +1,72 @@
+
+//  Select Element
+const hamburger = document.getElementById('hamburger');
+const navlinks = document.getElementById('show');
+
+// Select Buttons
+const bookBtn = document.querySelector('.btn-primary');
+const doctorsBtn = document.querySelector('.btn-secondary');
+
+// Select all features blocks
+const features = document.querySelectorAll('.feature')
+
+// footer newsletter form validation (JavaScript)
+const from = document.getElementById('footer-newsletter');
+const emailInput = document.getElementById('newsletter-email')
+const message = document.getElementById('newsletter-message')
+
+from.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const email = emailInput.value.trim();
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email.match(emailPattern)) {
+        message.style.color = "lightgreen";
+        message.textContent = "✅ Successfully subscribed!"
+        emailInput.value = "";// clear input
+    }
+    else{
+        message.style.color = "red";
+        message.textContent = "❌ Please enter a valid email address.";
+    }
+});
+
+
+// Add click event
+hamburger.addEventListener('click', () =>{
+    navlinks.classList.toggle('show');
+});
+
+// Scroll to features section
+bookBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    document.querySelector('.features').scrollIntoView({behavior: "smooth"});
+});
+
+// scroll to doctor section
+doctorsBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.doctors').scrollIntoView({behavior: "smooth"});
+});
+
+// Function to check visibility
+function showFeaturesOnScroll(){
+    features.forEach(feature => {
+        const rect = feature.getBoundingClientRect();
+        if(rect.top > window.innerHeight - 100 ){
+            feature.classList.add('show') //show visible class
+        }
+    });
+}
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
